66d767c92a72e6201b39b5c197c1e15233c9e6e1
Pablo Noguera <pabnomi@gmail.com>
2021-02-02 21:56:33 +0100

    Add conditionals to read variables from docker-compose secrets

--- dynhost.sh
+++ dynhost.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
+if [ $DYNHOST_DOMAIN_NAME_FILE ]; then
+    DYNHOST_DOMAIN_NAME=$(cat $DYNHOST_DOMAIN_NAME_FILE)
+fi
+if [ $DYNHOST_LOGIN_FILE ]; then
+    DYNHOST_LOGIN=$(cat $DYNHOST_LOGIN_FILE)
+fi
+if [ $DYNHOST_PASSWORD_FILE ]; then
+    DYNHOST_PASSWORD=$(cat $DYNHOST_PASSWORD_FILE)
+fi
+
 echo "Checking IP" >> dynhost.log
 
 IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d \")
 CURRENT_IP=$(dig +short $DYNHOST_DOMAIN_NAME)
 
-if [ "$IP" ]; then 
-    if [ "$CURRENT_IP" != "$IP" ]; then 
+if [ "$IP" ]; then
+    if [ "$CURRENT_IP" != "$IP" ]; then
         echo "Updating $DYNHOST_DOMAIN_NAME from $CURRENT_IP to $IP" >> dynhost.log
         curl --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "http://www.ovh.com/nic/update?system=dyndns&hostname=${DYNHOST_DOMAIN_NAME}&myip=${IP}" >> dynhost.log 2>&1
-    else 
+    else
         echo "No update required" >> dynhost.log
-    fi 
-else 
+    fi
+else
     echo "IP not found" >> dynhost.log
 fi
